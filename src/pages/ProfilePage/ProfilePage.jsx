5c12254cecc6efb9c431f44df76451ef05cd9bec
Paula Jardim <paulajardimf@gmail.com>
2023-04-08 18:09:50 -0300

    :sparkles: feat: ProfilePage

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,43 @@
-
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProfilePage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [userDetails, setUserDetails] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          `https://api.github.com/users/${params.username}`
+        );
+        setUserDetails(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+
   return (
-    <div>ProfilePage</div>
-  )
+    <>
+      {userDetails ? (
+        <>
+          <h1>Profile Page</h1>
+          <h1>{userDetails.name}</h1>
+          <img src={userDetails.avatar_url} alt={userDetails.name} />
+          <p>{userDetails.bio}</p>
+          <p>Followers: {userDetails.followers}</p>
+          <p>Following: {userDetails.following}</p>
+        </>
+      ) : (
+        <p>Carregando...</p>
+      )}
+    </>
+  );
 }
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import ProfilePage from '../pages/ProfilePage/ProfilePage'
 
-
 export default function Router() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/perfil/:username" element={<ProfilePage />} />
+        <Route path="/perfil/:username" element={<ProfilePage/>} />
       </Routes>
     </BrowserRouter>
   )
