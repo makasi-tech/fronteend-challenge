bac030175b82791942bb0191adc927b146a67f9c
ellenmariadev <ellenmariadev@gmail.com>
2023-04-10 15:10:08 -0300

    feat: add routes

---
+++ src/routes/root.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Profile, Home, ErrorPage } from "@/pages";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile/:username",
+    element: <Profile />,
+  },
+]);
