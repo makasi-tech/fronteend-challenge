8a6572074c093a8f8977ce8bc132d95d693e0f30
ellenmariadev <ellenmariadev@gmail.com>
2023-04-11 00:13:18 -0300

    feat: styling 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,3 +1,13 @@
+import S from "./error.module.css";
+import { useNavigate } from "react-router-dom";
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+  return (
+    <main className={S.main}>
+      <h1>ERROR 404</h1>
+      <p>The page you requested was not found!</p>
+      <button onClick={() => navigate("/")}>Go Back</button>
+    </main>
+  );
 };
