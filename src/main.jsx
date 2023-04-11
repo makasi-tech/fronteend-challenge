976f554cc26e22958a447791d95d96dd7c06586c
ellenmariadev <ellenmariadev@gmail.com>
2023-04-10 23:30:01 -0300

    feat: add global style

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/root";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
