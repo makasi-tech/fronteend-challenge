4d96e61449ba35932a71eaf9f6aede884044208a
ellenmariadev <ellenmariadev@gmail.com>
2023-04-10 23:33:31 -0300

    build: add react-icons + useMedia hook

---
+++ src/hooks/useMedia.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const useMedia = (media) => {
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener("resize", changeMatch);
+    return () => {
+      window.removeEventListener("resize", changeMatch);
+    };
+  }, [media]);
+  return match;
+};
