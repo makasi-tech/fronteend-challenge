55a83a843f47af3a64b36f61403acf463270354f
ellenmariadev <ellenmariadev@gmail.com>
2023-04-10 23:41:12 -0300

    feat: styling repository component

--- src/components/repository/index.jsx
+++ src/components/repository/index.jsx
@@ -1,20 +1,36 @@
 import P from "prop-types";
+import S from "./repository.module.css";
+import { RxStar } from "react-icons/rx";
+import { useMedia } from "@/hooks/useMedia";
+import { Link } from "react-router-dom";
 
 export const Repository = ({ repo }) => {
-  const { name, description, stargazers_count, updated_at } = repo;
+  const { name, description, stargazers_count, updated_at, html_url } = repo;
 
   const updated = updated_at.substring(0, 10);
   const givenDate = new Date(updated);
   const days = Math.ceil((Date.now() - givenDate) / (1000 * 60 * 60 * 24));
-  const timeAgo = days >= 30 ? `${Math.floor(days / 30)} months` : `${days} days`;
+  const timeAgo =
+    days >= 30 ? `${Math.floor(days / 30)} months` : `${days} days`;
 
+  const mobile = useMedia("(max-width: 320px)");
   return (
-    <div>
-      <h2>{name}</h2>
+    <ul className={S.repository} role="list">
+      <li>
+        <Link className={`${S.link} fw-bold fs-md`} to={html_url} target="_blank">
+          {name}
+        </Link>
+      </li>
       <p>{description}</p>
-      <h3>{stargazers_count}</h3>
-      <h3>Updated {timeAgo} ago</h3>
-    </div>
+      <ul className={`${S.info} ${mobile ? "fs-xs" : "fs-sm"}`}>
+        <li>
+          <RxStar />
+          {stargazers_count} stars
+        </li>
+        <li>● Updated {timeAgo} ago</li>
+      </ul>
+      <hr className={S.line} />
+    </ul>
   );
 };
 
@@ -24,5 +40,6 @@ Repository.propTypes = {
     description: P.string,
     stargazers_count: P.number,
     updated_at: P.string,
+    html_url: P.string,
   }),
 };
